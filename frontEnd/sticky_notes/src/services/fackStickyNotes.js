73be366936943c0f084f13353b2ed3dfad9b1847
divaymohan <divaymohan05@gmail.com>
2020-06-26 10:30:30 +0530

    designing schema for the project

---
+++ frontEnd/sticky_notes/src/services/fackStickyNotes.js
@@ -0,0 +1,27 @@
+const notes = [
+  {
+    userId: 1,
+    Notes: [
+      {
+        notesCatagory: "os",
+        note_id: 1,
+        Notes: ["hey i am divay mohan", "How are you", "i am fine"],
+        createdData: Date.now(),
+      },
+      {
+        notesCatagory: "os",
+
+        note_id: 2,
+        Notes: ["hey i am divay mohan", "How are you", "i am fine"],
+        createdData: Date.now(),
+      },
+      {
+        notesCatagory: "os",
+
+        note_id: 3,
+        Notes: ["hey i am divay mohan", "How are you", "i am fine"],
+        createdData: Date.now(),
+      },
+    ],
+  },
+];

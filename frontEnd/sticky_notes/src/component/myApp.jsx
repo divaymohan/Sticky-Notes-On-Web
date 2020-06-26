4c525cb5f58eff6a7d70cd681a665bffe63a886b
divaymohan <divaymohan05@gmail.com>
2020-06-26 10:57:44 +0530

    added fontawesome and bootstrap

---
+++ frontEnd/sticky_notes/src/component/myApp.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MyApp extends Component {
+  state = {};
+  render() {
+    return <h1>Hey This is my app..!!</h1>;
+  }
+}
+
+export default MyApp;
